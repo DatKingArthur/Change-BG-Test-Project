eba93cf0ba473751d2228a6445b1b3cdc936c343
Arthur <64805089+DatKingArthur@users.noreply.github.com>
2020-06-11 23:10:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+document.body.addEventListener('click', setBG)
+
+function setBG(e) {
+    if (e.target.id === 'red') {
+        document.body.style.backgroundColor = 'red'
+    } else if (e.target.id === 'orange') {
+        document.body.style.backgroundColor = 'orange'
+    } else if (e.target.id === 'yellow') {
+        document.body.style.backgroundColor = 'yellow'
+    } else if (e.target.id === 'green') {
+        document.body.style.backgroundColor = 'green'
+    } else if (e.target.id === 'blue') {
+        document.body.style.backgroundColor = 'blue'
+    } else if (e.target.id === 'purple') {
+        document.body.style.backgroundColor = 'purple'
+    } else {
+        document.body.style.backgroundColor = '#fff'
+    }
+}
